5d6815f0e9468f39082268fe8f21276c4eaf794f
fonsecapaulacf <fonseca.paula.cf@gmail.com>
2023-02-18 17:38:27 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
+
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <footer className="footer text-center"> Coded by Paula Fonseca </footer>
+      
+      
+      </div>
     </div>
   );
 }
