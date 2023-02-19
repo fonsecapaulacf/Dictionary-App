3944292a541399426c7d61dcb1d6d85d64c3a93f
fonsecapaulacf <fonseca.paula.cf@gmail.com>
2023-02-19 11:02:06 +0100

    added axios and handled response

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,24 @@
 import React, {useState} from "react";
+import axios from "axios";
 import './Dictionary.css';
 
 
 export default function Dictionary() {
     let[keyword, setKeyword] = useState ("");
 
-   function search(event) {
+   function handleResponse(response){
+    console.log(response.data);
+    }
+   
+    function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
-   }
+    
+   
+    let key=`a2ae14484420ao51b3294b9fbftb12ae`;
+    let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+   
+    axios.get(apiUrl).then(handleResponse) ;
+    }
 
    function handleKeywordChange(event){
     setKeyword(event.target.value);
