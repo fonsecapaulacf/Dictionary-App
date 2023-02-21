58c10a3267de87ec1bf2d966752f3837fd548560
fonsecapaulacf <fonseca.paula.cf@gmail.com>
2023-02-21 13:45:12 +0100

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -10,12 +10,13 @@ export default function Results(props){
     <h2>{props.results.word}</h2>
     <p>{props.results.phonetic}</p>
     
+    
     <h3>{props.results.meanings.map(function(meaning, index){
     if (index<3){
     return (
         <div key={index}> <Meaning meaning={meaning}/> </div> 
         );
-    }
+    } else return null;
    })
    }
    </h3>
